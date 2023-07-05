745f90c191786643a97e16018852c2389ae34299
Dundiev Oleh <dundev1904@gmail.com>
2023-07-05 17:37:13 +0200

    Cycles. Function.object

---
+++ Lisson1/main.js
@@ -0,0 +1,138 @@
+"use strict";
+/*console.log("Name");
+console.log("Name");*/
+/*console.log("Name");
+console.log("Hallo freunde");
+console.log("[ADDDFFE]",10+25);
+console.log(24-10);
+console.log("[Guten Tag]", 25/3);
+console.log("[Guten Abend]",37%36);
+console.log("[Tschüss]", 25**2);
+console.log("[String1]", "vasy "+" pety");
+console.log("[String2]", "Vasy"+22);
+console.log("[String3]","25"+56);
+console.log("[String5]", "48"+"22");
+console.log("[String5]", 25+36);*/
+//var string="Hallo Tschöss!";
+/*let name=("Oleh Dundiev");
+console.log("[NAME]", name);
+console.log("[Slice1]",name.slice(0 ,5));
+let hallo=(" Privet rodnoy");
+console.log("[Hallo]", hallo);
+console.log("[Itogo]", name + hallo)
+let nameHallo=(name +""+name+""+hallo);
+console.log("[NAMEhALLO]", nameHallo.slice(3, 15))*/
+//let null_value = null;
+//console.log("[null_value]", null_value) ;
+//let user_name=prompt("Enter yor name: ")
+//console.log("[user_name]", user_name);
+//alert("[Hallo]", null_value)
+//let confirmMessage=confirm("Hallo! Wei geht es");
+//console.log("[confirmMessadg]", confirmMessage);
+/*//! 1
+let GreetingByDefault=("Hallo! Schön dich zu zehen!");
+//! Sveta freunden
+let freundOleh="Oleh";
+let freundKir="Kir";
+let freundVika="Vika";
+//! Текущее имя прользователя
+let userName=prompt("Hello! Ihr Name?");
+if (userName===freundOleh||
+    userName===freundKir||
+    userName===freundVika
+    )
+    {
+    console.log(GreetingByDefault+userName);
+    let isConfirm= confirm("Willst du die Nachricht sehen?")
+    if (isConfirm===true){
+        alert(GreetingByDefault+userName)
+    }
+    else {
+        alert("Tschöss!")
+     }
+
+}
+else{
+    console.log("Tschöss!")
+}*/
+
+//! 2
+/*
+let GreetingByDefault=("Hallo! Schön dich kennenzulernen!");
+let nameOleh="Oleh";
+let nameSveta="Sveta";
+let nameVika="Vika";
+let nameKatja="Katja";
+let userName=prompt("Hallo! Wie heisen Sie?");
+if( userName===nameOleh||
+    userName===nameKatja||
+    userName===nameVika||
+    userName===nameSveta)
+    {
+        console.log(GreetingByDefault+userName);
+        let meinConfirm=("Möchten Sie nachricht sehen?");
+        if ( meinConfirm===true){ alert=(GreetingByDefault+userName)}
+        else{alert("Tschüss!")}
+    }
+    else{
+        console.log("Tschöss!")
+    }*/
+
+    //! 3
+   /* let StandartBegrüßen="Hallo! Schön dich kennenzulernen!";
+    let nameOleh="Oleh";
+    let nameSveta="Sveta";
+    let nameVika="Vika";
+    let nameKir="Kir";
+    let userName=prompt("Hallo! Wie heißen Sie?");
+    if(userName===nameOleh||
+        userName===nameKir||
+        userName===nameVika||
+        userName===nameSveta){
+            console.log(StandartBegrüßen+userName);
+            let meinConfirm=confirm("Möchten Sie nachricht sehen?");
+            if (meinConfirm===true){ alert(StandartBegrüßen+userName)}
+            else { alert("Tschööööössss!")}
+        }
+        else {alert("Tschöööösss!!")}
+       else {
+         console.log("Tschuss!")}*/
+            //! 5
+ /*let standartBegrüßen="Hallo! Schön dich kennenzulernen! Herzlich willkommen!"
+ let nameKir="Kir";
+ let nameSveta="Sveta";
+ let nameVika="Vika";
+ let nameOleh="Oleh";
+ let nameUser=prompt("Hallo! Wie heißt du!");
+ if(nameUser===nameKir||
+    nameUser===nameOleh||
+    nameUser===nameSveta||
+    nameUser===nameVika){
+        console.log(standartBegrüßen+nameUser);
+        let meinConfirm=confirm("Möchtest du nachricht sehen?");
+        if(meinConfirm===true){alert(standartBegrüßen+nameUser)}
+        else{alert("Tschööössss!!!")}
+    }
+    else{console.log("Пока!")}*/
+    //! 6
+  
+
+// let StandartSenden="Guten Abend! Herzlich Willkommen!";
+// let userKir="Kir";
+// let userVika="Vika";
+// let userOleh="Oleh";
+// let userSveta="Sveta";
+// let userName=prompt("Hallo! Wie heißt du?");
+// if(userName===userKir||
+//     userName===userOleh||
+//     userName===userSveta||
+//     userName===userVika){
+//         console.log(StandartSenden+userName);
+//         let meinConfirM=confirm("Möchtest du nachricht sehen?");
+//         if (meinConfirM===true){alert(StandartSenden+userName)}
+//         else{alert("Auf Wiedersehen!" )}
+//     }
+//     else{ console.log("Auf Wiedersehen!")}
+//     //! Hausaufgabe 1
+
+hjf
