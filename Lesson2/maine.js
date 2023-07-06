372b2864f8c334b3e071db8e885735d666570799
Dundiev Oleh <dundev1904@gmail.com>
2023-07-06 23:26:47 +0200

    6.07.23 JS калькулятор

--- Lesson2/maine.js
+++ Lesson2/maine.js
@@ -1,1 +1,64 @@
 "use strict"
+// !Калькулятор
+// let firstNumber=prompt("Введите первое число");
+// let secondNumber=prompt("Введите второе число");
+// let operator=prompt("Ведите +, -, *,/");1           
+// alert("Hallo")
+// let result=prompt(title,["default"]);
+// console.log(result)
+// result = prompt(["dfskgjsddj"], "jkdjksdj");
+// console.log(result)
+// let age=prompt("Wie alt bist du?", 100)
+// console.log(age)
+// // 'alert("Du bist ${age} jare!");'
+// alert(`Du bist ${age} jare!`);
+// let name=prompt("Wie heißt du?")
+//     alert( `Hallo ${name} !`);
+// let confirmName=confirm(`Du bist Oleh?`)
+// alert(confirmName)
+//  let name=prompt(`Ihre name?`);
+//  alert(`Hallo ${name}!`);
+//  let age=prompt(`Wie alt sind Sie?`)
+//  alert(`Sie sind ${age} jare alt`)
+//  let spilt=confirm(`Möchtest du spielen?`)
+//  alert(spilt)
+//! Калькулятор
+// let result=null;
+// let ersteNum=parseInt(prompt(`Введите первое число`));
+// let zweiteNum=parseInt(prompt(`Введите второе число`));
+// let operator=prompt(`Введите опирацию: +;-;*;%;/` );
+// if(operator===`+`){
+//     result=ersteNum+zweiteNum   
+// }else if(operator===`-`){
+//     result=ersteNum-zweiteNum
+// }else if(operator===`/`){
+//     result=ersteNum/zweiteNum
+// }
+// else if(operator===`*`){
+//     result=ersteNum*zweiteNum
+// }else if(operator===`%`){
+//     result=ersteNum/zweiteNum
+// }
+// console.log(result)
+//! 1
+let result=null;
+let ersteZahl=parseInt(prompt(`Geben Sie die erste Zahl ein`));
+let zweiteZahl=parseInt(prompt(`Geben Sie die zweite Zahl ein`));
+let operator=prompt(`Geben Sie einen Operator ein: "+" "-" "/" "%" "*"`)
+if(operator===`+`){
+    result=ersteZahl+zweiteZahl
+}
+else if(operator===`-`){
+    result=ersteZahl-zweiteZahl
+}else if(operator===`*`){
+    result=ersteZahl*zweiteZahl
+}else if(operator===`%`){
+    result=ersteZahl%zweiteZahl
+}
+if(!isNaN(result)){
+    console.log(`[result]`, result)
+}
+else{
+    alert(`Etwas ist schief gelaufen!`)
+}
+
