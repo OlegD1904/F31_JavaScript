7dfd307026d746e3f57545cc75c2621b04c7fe79
Dundiev Oleh <dundev1904@gmail.com>
2023-07-17 17:18:29 +0200

    Валидатор практика

---
+++ Практика/main.js
@@ -0,0 +1,52 @@
+`use strict`
+// console.log(`jdfkjdsf`)
+//!!!!!!!!!!VALIDATOR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //1 - Validation for : name and age
+     //Валидация: имя и возраст
+   //name - schould contain: . as a first character in the string
+     //имя - должно содержать: . как первый символ в строке     
+   //name -schould contain: ! as a last character in the string
+     //имя -должно содержать: ! как последний символ в строке
+   //2- age -user age should divide on 4 without part divide operation
+     //2-возраст-возраст пользователя должен делиться на 4 без операции деления на части
+// var validator={
+//     isValidName:false,
+//     isValidAge:false,
+// }
+// while(!validator.isValidName)
+// {
+//     var userName=prompt(`Введи имя`);
+//     if(userName[0]===`.`&& userName[userName.length-1]===`!`){
+//         validator.isValidName=true
+        
+//         console.log(`Hallo!`)
+//         alert(`Gut arbeit`)
+//         break
+//     }    
+// }
+// var validation={
+//     validName:false,
+//     validAge:false,
+// }
+// while(!validation.validName){
+//     var userName=prompt(`Ihr Name?`);
+//     if(userName[0]===`.`&& userName [userName.length-1]===`!`){
+//         validation.validName=true  
+//         break
+//     }
+
+// }
+
+// var validator={
+//     validName:false,
+//     validAge:false,
+// }
+// while(!validator.validName){
+//     userName= prompt(`Ihe name:`)
+//     if(userName[0]===`.` && userName[userName.length-1]===`!`){
+//         validator.validName=true
+//         console.log(`Hallo`)
+//         alert(`Hallo`)
+//         break
+//     }
+// }
